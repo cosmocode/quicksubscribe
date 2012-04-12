8866222d63bbbe719deac62ad6918026828d1e3a
Andreas Gohr <gohr@cosmocode.de>
2012-04-12 16:14:56 +0200

    added englich translation

---
+++ lang/en/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['js']['subscr_in_progress'] = 'Creating subscription …';
+$lang['js']['is_subscr']          = 'You subscribed to this page via the %s namespace.';
+$lang['js']['del_subscr']         = 'Do you want to delete this subscription?';
+$lang['js']['del_subscr_button']  = 'Delete';
+$lang['js']['edit_subscr']        = 'Do want to manage your subscription settings?';
+$lang['js']['edit_subscr_button'] = 'Edit';
+$lang['js']['subscribe']          = 'Subscribe to changes';
+$lang['js']['title']              = 'Subscription';
+$lang['js']['sub_succ']           = 'The subscription was created.<br />You subscribed the %s namespace.';
+$lang['js']['sub_fail']           = 'The subscription couldn\'t be created.';
+$lang['js']['unsub_succ']         = 'The subscription was removed.<br />You\'re no longer subscribed to the %s namespace.';
+$lang['js']['unsub_fail']         = 'Could not delete the subscription.';
